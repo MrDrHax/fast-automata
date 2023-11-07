27809f32771fafa595eef7bb035734c8813809a1
MrDrHax <alexfh2001@gmail.com>
2023-11-06 18:24:28 -0600

    trying new things

--- setup.py
+++ setup.py
@@ -2,26 +2,39 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 import sys, os
 import setuptools
-import pybind11
 
 __version__ = '0.3.0'
 
-def get_pybind_include(user=False):
-    return pybind11.get_include(user)
+using_pybind = False
 
-# Define Pybind11 extension
-ext_modules = [
-    Extension(
-        'fastautomata.fastautomata_clib',
-        ['fastautomata/include/fastautomata/bindings.cpp'],
-        include_dirs=[
-            # Path to pybind11 headers
-            get_pybind_include(),
-            get_pybind_include(user=True),
-        ],
-        language='c++'
-    ),
-]
+try:
+    from fastautomata import fastautomata_clib
+
+except ImportError:
+    print("Could not import fastautomata_clib, please install pybind11 and a compiler to compile liraries. Attempting to compile")
+
+    import pybind11
+
+    def get_pybind_include(user=False):
+        return pybind11.get_include(user)
+
+
+
+    # Define Pybind11 extension
+    ext_modules = [
+        Extension(
+            'fastautomata.fastautomata_clib',
+            ['fastautomata/include/fastautomata/bindings.cpp'],
+            include_dirs=[
+                # Path to pybind11 headers
+                get_pybind_include(),
+                get_pybind_include(user=True),
+            ],
+            language='c++'
+        ),
+    ]
+
+    using_pybind = True
 
 setup(
     name="fast-automata",
@@ -37,8 +50,8 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/MrDrHax/fast-automata",
-    install_requires=["pyglet", "pydantic", "pybind11"],
+    install_requires=["pyglet", "pydantic"],
     python_requires='>=3.10',
     license="GPLv3",
-    ext_modules = ext_modules,
+    ext_modules = ext_modules if using_pybind else None,
 )
