f38e4640b53c195f858a95f8dbb25aea22fc4406
MrDrHax <alexfh2001@gmail.com>
2023-11-27 00:16:10 -0600

    Fix bugs and add new features and documentation

--- TODO.md
+++ TODO.md
@@ -1,4 +1,22 @@
 # Bugs
 
 - [ ] When adding a new thing in a pos that is put of bounds, it still works... Should throw error!
-- [ ] 
+- [ ] If you add the drawer after agents are added, there is an error (add all agents after startup)
+
+# Features
+
+- [ ] make the agents reset by board. This will allow multiple boards at the same time
+- [ ] Figure out a way to parallelize boards. (IDK, probably it's something about the GIL)
+- [ ] Add a list of objects of certain type (like colors)
+- [ ] Use python exceptions. (Currently using c++ exceptions)
+- [ ] Make more detailed exceptions
+
+# Docs
+
+- [ ] Actually make nice docs
+
+# Build
+
+- [ ] Figure out how tf to automize wheels on windows
+- [ ] Make github actions that automatically make a new post
+- [ ] Eventually upload to PyPi
--- fastautomata/ClassTypes.py
+++ fastautomata/ClassTypes.py
@@ -1,5 +1,15 @@
+from collections.abc import Callable
+from typing import Any
 from . import fastautomata_clib
 from pydantic import BaseModel
+import json, enum
+
+class PosJSONEnconderTypes(enum.Enum):
+    ARRAY = 0
+    TUPLE = 1
+    DICT = 2
+
+pos_json_encoder_config = PosJSONEnconderTypes.TUPLE
 
 class Pos(fastautomata_clib.Pos):
     '''
@@ -14,6 +24,54 @@ class Pos(fastautomata_clib.Pos):
     def __repr__(self) -> str:
         return f'<Pos({self.x}, {self.y})>'
     
+    def to_array(self) -> list[int]:
+        return [self.x, self.y]
+    
+    def to_tuple(self) -> tuple[int, int]:
+        return (self.x, self.y)
+    
+    def to_dict(self) -> dict[str, int]:
+        return {"x": self.x, "y": self.y}
+    
+    @classmethod
+    def from_array(cls, array: list[int]) -> 'Pos':
+        return cls(array[0], array[1])
+
+    @classmethod
+    def from_tuple(cls, tuple: tuple[int, int]) -> 'Pos':
+        return cls(tuple[0], tuple[1])
+
+    @classmethod
+    def from_dict(cls, dict: dict[str, int]) -> 'Pos':
+        return cls(dict["x"], dict["y"]) 
+
+class PosJSON(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Pos):
+            global pos_json_encoder_config
+            if pos_json_encoder_config == PosJSONEnconderTypes.ARRAY:
+                return obj.to_array()
+            elif pos_json_encoder_config == PosJSONEnconderTypes.DICT:
+                return obj.to_dict()
+            elif pos_json_encoder_config == PosJSONEnconderTypes.TUPLE:
+                return obj.to_tuple()
+            else:
+                raise TypeError("Invalid PosJSONEnconderTypes")
+            
+        if isinstance(obj, fastautomata_clib.Pos):
+            return (obj.x, obj.y)
+            
+        return super().default(obj)
+    
+    @classmethod
+    def decode(cls, obj):
+        if isinstance(obj, list):
+            if len(obj) == 2 and isinstance(obj[0], int) and isinstance(obj[1], int):
+                return Pos.from_array(obj)
+            return [Pos.from_array(x) for x in obj]
+
+        return obj
+
 class PosModel(BaseModel):
     x: int
     y: int
